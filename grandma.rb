bc9c64162d3aa63e1e39c332c77080fbed8d0c6c
Megan Horsfield <meganthadev@gmail.com>
2020-02-14 02:37:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,6 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase) 
-  puts phrase
    if phrase == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"
     elsif phrase == phrase.downcase
