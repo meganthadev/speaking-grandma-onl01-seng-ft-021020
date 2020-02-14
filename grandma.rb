a5de75411c9da3efd8443a59bf1ec1d6739c961a
Megan Horsfield <meganthadev@gmail.com>
2020-02-14 02:30:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(phrase = "Hi Nana, how are you?")
   puts phrase
   if phrase == phrase.toUpperCase 
     return "NO, NOT SINCE 1938!"
-  elsf phrase == "I LOVE YOU GRANDMA!" 
+  elsif phrase == "I LOVE YOU GRANDMA!" 
             return "I LOVE YOU TOO PUMPKIN!"
   else return  "HUH?! SPEAK UP, SONNY!"         
 end
