fef08c149fc27f1c74118d4391c7ec41aa210299
Megan Horsfield <meganthadev@gmail.com>
2020-02-14 02:27:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -20,4 +20,4 @@ def speak_to_grandma(phrase = "Hi, Grandma!")
    if (phrase == "I LOVE YOU GRANDMA!") {
             return "I LOVE YOU TOO PUMPKIN!"
   else return  "HUH?! SPEAK UP, SONNY!"         
-end
+end;
