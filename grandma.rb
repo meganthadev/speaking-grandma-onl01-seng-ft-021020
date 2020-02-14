de9ac60aa77f621c4e4d22f2dd9cbddc8639faf0
Megan Horsfield <meganthadev@gmail.com>
2020-02-14 02:32:13 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -20,4 +20,7 @@ def speak_to_grandma(phrase = "Hi Nana, how are you?")
   		return "HUH?! SPEAK UP, SONNY!"
   	elsif phrase == phrase.upcase
       return "NO, NOT SINCE 1938!"
+    end  
 end
+
+speak_to_grandma("HELLO!")
